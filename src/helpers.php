333ea59ad75353b26a54b63cefa3672554043f28
maryamnabiyan <maryam.nbyn@gmail.com>
2020-11-07 16:34:13 +0330

    add helper function

---
+++ src/helpers.php
@@ -0,0 +1,15 @@
+<?php
+
+if (! function_exists('jam')) {
+    /**
+     * Add an element to an array using "dot" notation if it doesn't exist.
+     *
+     * @param $number1
+     * @param $number2
+     * @return array
+     */
+    function jam($number1, $number2)
+    {
+        return $number1 + $number2;
+    }
+}
