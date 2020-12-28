b5f2c49244b61f2dfa4fe3275e76189577275a6a
m.nabiyan <maryam.nbyn@gmail.com>
2020-12-28 14:26:47 +0330

    delete jam and tafrigh helper function

--- src/helpers.php
+++ src/helpers.php
@@ -1,33 +1,5 @@
 <?php
 
-if (! function_exists('jam')) {
-    /**
-     * Add an element to an array using "dot" notation if it doesn't exist.
-     *
-     * @param $number1
-     * @param $number2
-     * @return array
-     */
-    function jam($number1, $number2)
-    {
-       tafriq($number1, $number2);
-    }
-}
-
-
-if (! function_exists('tafriq')) {
-    /**
-     * Add an element to an array using "dot" notation if it doesn't exist.
-     *
-     * @param $number1
-     * @param $number2
-     * @return array
-     */
-    function tafriq($number1, $number2)
-    {
-        return $number1 - $number2;
-    }
-}
 
 if (! function_exists('resolve_pagination_links')) {
     /**
@@ -63,10 +35,7 @@ if (! function_exists('resolve_pagination_links')) {
         return $response;
     }
 
-
-
 }
-
 
 if (! function_exists('url_without_query_string')) {
     /**
