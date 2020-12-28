620acf60d426faf09c88f0bd83efe63a68a65ba8
m.nabiyan <maryam.nbyn@gmail.com>
2020-12-28 14:09:10 +0330

    add url_without_query_string helper function

--- src/helpers.php
+++ src/helpers.php
@@ -10,6 +10,21 @@ if (! function_exists('jam')) {
      */
     function jam($number1, $number2)
     {
+       tafriq($number1, $number2);
+    }
+}
+
+
+if (! function_exists('tafriq')) {
+    /**
+     * Add an element to an array using "dot" notation if it doesn't exist.
+     *
+     * @param $number1
+     * @param $number2
+     * @return array
+     */
+    function tafriq($number1, $number2)
+    {
         return $number1 - $number2;
     }
 }
@@ -23,26 +38,46 @@ if (! function_exists('resolve_pagination_links')) {
      */
     function resolve_pagination_links(array $response)
     {
-        $response['links']['first'] = str_replace(strtok($response['links']['first'], '?'),
+        $response['links']['first'] = str_replace(url_without_query_string($response['links']['first']),
             request()->url(),
             $response['links']['first']);
 
-        $response['links']['last'] = str_replace(strtok($response['links']['last'], '?'),
+        $response['links']['last'] = str_replace(url_without_query_string($response['links']['last']),
             request()->url(),
             $response['links']['last']);
 
-        $response['links']['next'] = !empty(str_replace(strtok($response['links']['next'], '?'),
+        $response['links']['next'] = !empty(str_replace(url_without_query_string($response['links']['next']),
             request()->url(),
-            $response['links']['next'])) ? str_replace(strtok($response['links']['next'], '?'),
+            $response['links']['next'])) ? str_replace(url_without_query_string($response['links']['next']),
             request()->url(),
             $response['links']['next']) : null;
 
-        $response['links']['prev'] = !empty(str_replace(strtok($response['links']['prev'], '?'),
+        $response['links']['prev'] = !empty(str_replace(url_without_query_string($response['links']['prev']),
             request()->url(),
-            $response['links']['prev'])) ? str_replace(strtok($response['links']['prev'], '?'),
+            $response['links']['prev'])) ? str_replace(url_without_query_string($response['links']['prev']),
             request()->url(),
             $response['links']['prev']) : null;
 
-        $response['meta']['path'] = strtok($response['links']['first'], '?');
+        $response['meta']['path'] = url_without_query_string($response['links']['first']);
+
+        return $response;
+    }
+
+
+
+}
+
+
+if (! function_exists('url_without_query_string')) {
+    /**
+     * Resolve pagination links.
+     *
+     * @param $url
+     *
+     * @return string
+     */
+    function url_without_query_string($url)
+    {
+        return strtok($url, '?');
     }
 }
